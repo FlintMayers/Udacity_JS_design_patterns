1a6274a86a1a0389fb8d6da4ebf3c58d36e3ad36
MB1 <mb1@MB1s-MacBook-Pro.local>
2017-09-06 11:42:48 +0300

    scalable cat clicker

--- script.js
+++ script.js
@@ -4,30 +4,58 @@ var domReady = function(callback) {
 
 domReady(function() {
 
-    function clicker(cat, counter, number){
-      cat.addEventListener('click', function(){
-        number++;
-        counter.innerHTML = number;
+    //array of cats
+    var cats = ['Princess', 'Tom', 'Couple', 'Pirate', 'Amnesty'];
+
+    var select = document.getElementById('cats');
+
+    for(var i = 0; i < cats.length; i++){
+      //generate select options from cat names
+      select.options[select.options.length] = new Option(cats[i], 'cat' + i);
+
+      //display selected cat and hide all the others
+      select.addEventListener('change', function(){
+          for(var i = 0; i < cats.length; i++){
+            var catId = 'cat' + i;
+            var cat = document.getElementById(catId);
+            cat.style.display = 'none';
+          }
+
+          var currentCat = document.getElementById(this.value);
+          currentCat.style.display = 'block';
       });
+
+      //create a unique container for each cat:)
+      var catDiv = document.createElement('div');
+      catDiv.setAttribute('id', 'cat' + i);
+      catDiv.setAttribute('class', 'catContainer');
+      document.body.appendChild(catDiv);
+
+      //prints the names
+      var title = document.createElement("h1");
+      var titleText = document.createTextNode(cats[i]);
+      title.appendChild(titleText);
+      catDiv.appendChild(title);
+
+      //prints the counter
+      var counter = document.createElement('h2');
+      var counterText = document.createTextNode("0");
+      counter.appendChild(counterText);
+      catDiv.appendChild(counter);
+
+      // prints the photos
+      var catPhoto = document.createElement("img");
+      catPhoto.src = './img/cat' + i + '.jpg';
+      catDiv.appendChild(catPhoto);
+
+      //increment counter after clicking cat photo
+      catPhoto.addEventListener('click', (function(currentCounter){
+        return function(){
+            var currentValue = parseInt(currentCounter.innerHTML);
+            currentCounter.innerHTML = currentValue + 1;
+        }
+      })(counter));
+
     }
-
-    //kitty1 data
-    var kitty1 = document.getElementById('kitty');
-    var counter1 = document.getElementById('counter');
-    var number1 = counter.innerHTML;
-    var name = document.getElementById('name');
-    name.innerHTML = 'Princess';
-
-    // kitty2 data
-    var kitty2 = document.getElementById('kitty2');
-    var counter2 = document.getElementById('counter2');
-    var number2 = counter2.innerHTML;
-    var name2 = document.getElementById('name2');
-    name2.innerHTML = 'Toruk';
-
-
-    
-    clicker(kitty1, counter1, number1);
-    clicker(kitty2, counter2, number2);
 
 });
